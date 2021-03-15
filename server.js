865408ad9820c041f1dfc6bfab9ad1333fdb4624
Mahesh Ranpise <mahesh8276@gmail.com>
2021-03-15 14:40:37 +0530

    V1

--- server.js
+++ server.js
@@ -34,6 +34,10 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
+app.get('/',(req,res) =>{
+  res.send("Fitness Logger API")
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
